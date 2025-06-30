52d56da001d63746a14130b70cf5ac9a016ff72d
Eveline-97 <eveline.vervliet@hotmail.com>
2025-06-30 16:22:49 +0200

    chapters

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const next = document.getElementById('next');
+const home = document.getElementById('home');
+const chapters = document.getElementById('chapters');
+const h1 = document.getElementsByTagName('h1');
+
+// go to next chapter...
+if (chapters) {
+    chapters.onclick = () => location.replace('../../chapters.html');
+}
+if (home) {
+    home.onclick = () => location.replace('../../index.html');
+}
+h1[0].onclick = () => location.replace('../../index.html');
